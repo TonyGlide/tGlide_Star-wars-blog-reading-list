b1395ab89f6ed8108ad324718bf1b8b7ff80fe4b
TonyGlide <162181740+TonyGlide@users.noreply.github.com>
2024-05-18 00:28:02 +0000

    u1

--- src/js/layout.js
+++ src/js/layout.js
@@ -3,8 +3,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 
 import { Home } from "./views/home";
-import { Demo } from "./views/demo";
-import { Single } from "./views/single";
+// import { Demo } from "./views/demo";
+// import { Single } from "./views/single";
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
@@ -23,9 +23,9 @@ const Layout = () => {
 					<Navbar />
 					<Routes>
 						<Route path="/" element={<Home />} />
-						<Route path="/demo" element={<Demo />} />
+						{/* <Route path="/demo" element={<Demo />} />
 						<Route path="/single/:theid" element={<Single />} />
-						<Route path="*" element={<h1>Not found!</h1>} />
+						<Route path="*" element={<h1>Not found!</h1>} /> */}
 					</Routes>
 					<Footer />
 				</ScrollToTop>
--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,45 +1,47 @@
 const getState = ({ getStore, getActions, setStore }) => {
+	
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			people: [],
+			planets: [],
+			vehicles: [],
+			favorites: []
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
+			getPeople: async () => {
+			 	 const response = await fetch("https://www.swapi.tech/api/people/");
+			if(!response.ok) {
+				throw new Error(response.status, response.statusText);
+			}	
+				 const data = await response.json();
+			 	 console.log(data.results);
+				 setStore({people: data.results});
+			
 			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
+			getPeopleDetails: () => {},
+			getPlanets: async () => {
+				 const response = await fetch("https://www.swapi.tech/api/planets/");
+				if(!response.ok) {
+					throw new Error(response.status, response.statusText);
+			}	
+				 const data = await response.json();
+				 setStore({planets: data.results});
 			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+			getPlanetsDetails: () => {},
+			getVehicles: async () => {
+				const response = await fetch("https://www.swapi.tech/api/vehicles/");
+				if(!response.ok) {
+					throw new Error(response.status, response.statusText);
+			}	
+				 const data = await response.json();
+				 setStore({planets: data.results});
+			},
+			getVehiclesDetails: () => {},
+			getFavorites: async () => {},
+			getfavoritesDetails: () => {},
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
 			}
 		}
 	};
-};
 
-export default getState;
+	export default getState;
